4c2fc5f363cdcdf9bc3dca2ee3c743d9ff949fc8
2cjh2 <42553301+2cjh2@users.noreply.github.com>
2018-08-24 13:28:06 -0400

    Update loader.php

--- loader.php
+++ loader.php
@@ -29,95 +29,14 @@ if ($_SESSION['type'] == "viewer") {
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
         <title>Loader</title>
-        <style type="text/css">
-            body {
-                font-size: 15px;
-                color: #343d44;
-                font-family: "segoe-ui", "open-sans", tahoma, arial;
-                padding: 0;
-                margin: 0;
-            }
-            table {
-                margin: auto;
-                font-family: "Lucida Sans Unicode", "Lucida Grande", "Segoe Ui";
-                font-size: 12px;
-            }
-
-            h1 {
-                margin: 25px auto 0;
-                text-align: center;
-                text-transform: uppercase;
-                font-size: 17px;
-            }
-
-            table td {
-                transition: all .5s;
-            }
-
-            /* Table */
-            .data-table {
-                border-collapse: collapse;
-                font-size: 14px;
-                min-width: 537px;
-            }
-
-            .data-table th, 
-            .data-table td {
-                border: 1px solid #e1edff;
-                padding: 7px 17px;
-            }
-            .data-table caption {
-                margin: 7px;
-            }
-
-            /* Table Header */
-            .data-table thead th {
-                background-color: #508abb;
-                color: #FFFFFF;
-                border-color: #6ea1cc !important;
-                text-transform: uppercase;
-            }
-
-            /* Table Body */
-            .data-table tbody td {
-                color: #353535;
-            }
-            .data-table tbody td:first-child,
-            .data-table tbody td:nth-child(4),
-            .data-table tbody td:last-child {
-                text-align: right;
-            }
-
-            .data-table tbody tr:nth-child(odd) td {
-                background-color: #f4fbff;
-            }
-            .data-table tbody tr:hover td {
-                background-color: #ffffa2;
-                border-color: #ffff0f;
-            }
-
-            /* Table Footer */
-            .data-table tfoot th {
-                background-color: #e5f5ff;
-                text-align: right;
-            }
-            .data-table tfoot th:first-child {
-                text-align: left;
-            }
-            .data-table tbody td:empty
-            {
-                background-color: #ffcccc;
-            }
-        </style>       
+        
     </head>
     <body>
         <form name="form" action="tableTools.php" method="POST" enctype="multipart/form-data">
             <input type="file" name="file" accept=".csv">   
             <input type ="submit" name="flexible_update" value="Flexible Update"> 
             <input type="submit" name="append" value="Append">  
-            /<!-- this would redirect directly to different page
-             <input type="submit" formaction="append.php" name="append" value="Append"> 
-             -->
+           
             <input type="submit" name="full_update" value="Full Update">
         </form> 
 
@@ -131,17 +50,12 @@ if ($_SESSION['type'] == "viewer") {
                         <input type="submit" name="submit" style="float:right" value="Restore from file">
              </form>
         
-         <form name="form" action="Download.php" method="POST" enctype="multipart/form-data">
+			<form name="form" action="Download.php" method="POST" enctype="multipart/form-data">
                 <input type="submit" name="full_download" style="float:right" value="Full Download">
             </form> 
 
-          
-
-
-             <button><a href="Summarizer.php">Main Page</a></button>
-             
-      <!-- most of the blow is probably garbage -->       
-             
+			<button><a href="Summarizer.php">Main Page</a></button>
+         
                 <?php
                         if ($_SESSION['type'] == "Admin" || $_SESSION["type"] == "Programmer") {
                             echo '
